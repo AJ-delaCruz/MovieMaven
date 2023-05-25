c28dd9e35690931eeb89d8a652e5cdb44d684fe1
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-05-24 23:41:29 -0700

    initialize Spring Boot project

---
+++ src/main/java/com/project/moviemaven/MovieMavenApplication.java
@@ -0,0 +1,13 @@
+package com.project.moviemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/moviemaven/MovieMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moviemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
