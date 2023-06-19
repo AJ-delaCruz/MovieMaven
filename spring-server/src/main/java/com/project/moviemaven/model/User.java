5078415c17f9af40a0a2baf9627229f05540b025
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-18 18:45:49 -0700

    implement UserDetails in User class, add enum Role

--- spring-server/src/main/java/com/project/moviemaven/model/User.java
+++ spring-server/src/main/java/com/project/moviemaven/model/User.java
@@ -1,13 +1,18 @@
 package com.project.moviemaven.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,12 +20,41 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
+@Builder
+public class User implements UserDetails {
 
     @Id
     private ObjectId id;
     private String username;
     private String password;
+    @Builder.Default
     private List<ObjectId> watchList = new ArrayList<>();
+    @Builder.Default
+    private Role role = Role.ROLE_USER; // Default role
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
