4c29985aae474669ed3e3621397c6cdad68c679e
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-11 23:45:19 -0700

    update unit test for retrieving user's watchlist

--- spring-server/src/test/java/com/project/moviemaven/service/WatchListServiceUnitTest.java
+++ spring-server/src/test/java/com/project/moviemaven/service/WatchListServiceUnitTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.project.moviemaven.model.Movie;
+import com.project.moviemaven.model.Role;
 import com.project.moviemaven.model.User;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,17 +47,21 @@ public class WatchListServiceUnitTest {
         Long movieId = 2L;
         Long movieId2 = 3L;
 
-        Movie movie1 = new Movie(movieId, "superman", null, null);
-        Movie movie2 = new Movie(movieId2, "batman", null, null);
-        ArrayList<Long> watchList = new ArrayList<>();
-        watchList.add(movieId);
-        watchList.add(movieId2);
-        User user = new User(userId, null, null, watchList, null);
+        Set<User> users = new HashSet<>();
+
+        Movie movie1 = new Movie(1L, movieId, "superman", "2005-06-15", users);
+        Movie movie2 = new Movie(2L, movieId2, "batman", "2008-07-18", users);
+
+        Set<Movie> watchList = new HashSet<>();
+        watchList.add(movie1);
+        watchList.add(movie2);
+
+        User user = new User(userId, "username", "password", watchList, Role.ROLE_USER);
 
         // mock the behavior of userService and movieService
         when(userService.getUserById(userId)).thenReturn(user);
-        when(movieService.getMovie(movieId)).thenReturn(movie1);
-        when(movieService.getMovie(movieId2)).thenReturn(movie2);
+        when(movieService.getMovieFromDb(movieId)).thenReturn(movie1);
+        when(movieService.getMovieFromDb(movieId2)).thenReturn(movie2);
 
         List<Movie> movieList = watchListService.getWatchList(user.getId());
         assertEquals(movieList.size(), 2);
