aec2b57862256f25550a193fc94c2c0573fc36ce
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-06 12:20:59 -0700

    implement Context API for AuthContext with custom hook and provider

---
+++ react-client/src/contextAPI/AuthContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, useState, useContext, ReactNode } from "react";
+
+//define auth context type to share
+interface AuthContextType {
+    currentUser: string | null;
+    login: (token: string) => Promise<void>; //async
+    logout: () => void;
+}
+
+// context object for authentication. Initialize context with undefined (or null)
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+// export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+
+//make custom hook for auth context
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error('useAuthContext must be used within an AuthProvider');
+    }
+
+    return context;
+}
+
+// define the prop types for AuthProvider
+interface AuthProviderProps {
+    children: ReactNode; // accept type that can be rendered in React
+}
+
+
+// create the component to share states to its children components
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState<string | null>(localStorage.getItem('token'));
+
+    const login = async (token: string) => {
+        localStorage.setItem("token", token);
+        setCurrentUser(token);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setCurrentUser(null);
+    };
+
+    // Provide the shared state to the children components
+    return (
+        <AuthContext.Provider value={{ currentUser, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
