0879569754bf8806821a91b0e6c4844386e746be
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-06 12:29:47 -0700

    integrate useAuthContext to handle client-side authentication

--- react-client/src/pages/Login/Login.tsx
+++ react-client/src/pages/Login/Login.tsx
@@ -1,20 +1,34 @@
-import { FormEvent, useState } from "react";
-import { backendUrl } from "../../config";
-import axios from "axios";
+import { useState, useEffect } from "react";
+import { backendUrl } from "../../utils/config";
+import axios, { AxiosError } from "axios";
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
+import { CircularProgress } from '@mui/material';
+import { useAuthContext } from "../../contextAPI/AuthContext";
+import { ERROR_MESSAGES } from "../../utils/errorMessage";
 
-const Login = () => {
+// type AxiosError = {  // custom
+//     response?: {
+//         data?: string;
+//     };
+// };
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+const Login: React.FC = () => {
+
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const { currentUser, login } = useAuthContext(); //access the custom hook for auth context shared state
     const navigate = useNavigate();
 
 
-    const submitLogin = async (e: FormEvent) => {
+    // const submitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    const submitLogin = async (e: React.FormEvent) => {
+        setIsLoading(true);
+
         e.preventDefault();
 
         try {
@@ -24,22 +38,38 @@ const Login = () => {
             });
 
             console.log(res.data);
+            const { accessToken } = res.data;
+            await login(accessToken)
 
             // Navigate to home page after successful login
             navigate('/');
         } catch (err) {
             console.log(err);
             // setErrorMessage(err.response.data.message);
+            const error = err as AxiosError;
+
+            if (error.response && typeof error.response.data === 'string') {
+                setErrorMessage(error.response.data);
+            } else {
+                setErrorMessage(ERROR_MESSAGES.UNEXPECTED);
+            }
+        } finally {
+            setIsLoading(false);
         }
 
 
     };
 
+    //navigate to the homepage ('/') when the currentUser state is updated after succesful login
+    useEffect(() => {
+        if (currentUser) {
+            navigate('/');
+        }
+    }, [currentUser, navigate]);
+
+
     return (
         <div className='grid-box'>
-
-            <div className='auth-box'>
-            </div>
             <div className='form'>
                 <div className='auth-header'>
                     <h3>Login here</h3>
@@ -49,7 +79,6 @@ const Login = () => {
                         id='outlined-basic'
                         name="username"
                         label='Username'
-                        // placeholder='username'
                         variant='outlined'
                         autoComplete='username'
                         autoFocus
@@ -70,13 +99,15 @@ const Login = () => {
                         }}
                     />
 
-
-                    <Button type='submit' variant='contained'>
+                    <Button type='submit' variant='contained' disabled={isLoading}>
                         Login
                     </Button>
                 </form>
-                {errorMessage && <p>{errorMessage}</p>}
 
+
+                {isLoading && <CircularProgress />}
+
+                {errorMessage && <p>{errorMessage}</p>}
 
                 <div className='button-container'>
                     <span>Don't have an account?</span>
