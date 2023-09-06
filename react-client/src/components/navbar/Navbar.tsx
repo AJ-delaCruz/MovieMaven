d1c60cb3c86654789e201a58ecf3e1e286b576ca
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-06 13:39:55 -0700

    add Navbar component with menu for profile, settings, logout, and notification for later

---
+++ react-client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,143 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+    IconButton, Badge, ListItemText, Menu, MenuItem, ListItemIcon
+} from '@mui/material';
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import Logout from '@mui/icons-material/Logout';
+import Settings from '@mui/icons-material/Settings';
+import Person from '@mui/icons-material/Person';
+// import { NotificationContext } from '../../contextAPI/NotificationContext'; //TODO
+import { useAuthContext } from '../../contextAPI/AuthContext';
+// import "./navbar.scss";
+
+type NotificationEvent = {
+    message: string;
+};
+
+const Navbar: React.FC = () => {
+    const [notificationAnchorEl, setNotificationAnchorEl] = useState<null | HTMLElement>(null);
+    const [profileAnchorEl, setProfileAnchorEl] = useState<null | HTMLElement>(null);
+
+    const { logout } = useAuthContext();
+    // const { notificationCount, notificationEvents, markAsRead, markAllAsRead } = useContext(NotificationContext); //TODO
+    const navigate = useNavigate();
+
+    const notificationOpen = Boolean(notificationAnchorEl);
+    const profileOpen = Boolean(profileAnchorEl);
+
+    const handleNotificationIconMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+        setNotificationAnchorEl(event.currentTarget);
+    };
+
+    const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+        setProfileAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setNotificationAnchorEl(null);
+        setProfileAnchorEl(null);
+    };
+
+    const handleLogout = () => {
+        handleClose();
+        logout();
+    };
+
+    const handleProfile = () => {
+        handleClose();
+        navigate('/profile');
+    };
+
+    const handleSettings = () => {
+        handleClose();
+        navigate('/settings');
+    };
+
+    const handleNotificationClick = (index: number) => {
+        // markAsRead(index);
+    };
+
+    return (
+        <div className="navbar">
+
+            <div className="left">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">MovieMaven</span>
+                </Link>
+            </div>
+
+            <div className="right">
+
+                <IconButton color="inherit" onClick={handleNotificationIconMenuClick}>
+                    {/* <Badge badgeContent={notificationCount} color="primary" > */}
+                    <Badge badgeContent={1} color="primary" >
+                        <NotificationsOutlinedIcon className="icon-cls" style={{ marginRight: "-9px" }} />
+
+                    </Badge>
+                </IconButton>
+
+
+                <div className="leftRight" onClick={handleProfileClick}>
+                    <PersonOutlinedIcon className="icon-cls" />
+                </div>
+
+
+                {/* Notifications menu */}
+                <Menu anchorEl={notificationAnchorEl} open={notificationOpen} onClose={handleClose}>
+
+                    {/* {notificationEvents.length > 0 ? (
+                        [
+                            notificationEvents.map((event, index) => (
+                                <MenuItem key={index} onClick={() => handleNotificationClick(index)}>
+                                    <ListItemIcon>
+                                        <NotificationsActiveIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={event.message} />
+                                </MenuItem>
+                            )),
+                            <MenuItem key="mark-all" onClick={markAllAsRead}>
+                                <ListItemText secondary="Mark all as read" align="center" />
+                            </MenuItem>
+                        ]
+                    ) : ( */}
+                    <MenuItem disabled>
+
+                        <ListItemText primary="No notifications" />
+                    </MenuItem>
+                    {/* )} */}
+                </Menu>
+
+
+                {/* profile menus */}
+                <Menu anchorEl={profileAnchorEl} open={profileOpen} onClose={handleClose} >
+
+                    <MenuItem onClick={handleProfile}>
+                        <ListItemIcon>
+                            <Person fontSize="small" />
+                        </ListItemIcon>
+                        Profile
+                    </MenuItem>
+
+                    <MenuItem onClick={handleSettings}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+
+                    <MenuItem onClick={handleLogout}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
