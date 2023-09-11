da8ca8702aa7a076ef46fba4fbb42affec1b45e9
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-11 13:03:11 -0700

    display movie preview when hovered

--- react-client/src/components/movie/Movie.tsx
+++ react-client/src/components/movie/Movie.tsx
@@ -1,21 +1,50 @@
+import { Link } from 'react-router-dom';
 import { MovieType } from '../../types/movie';
 import "./movie.scss";
+import { useState } from 'react';
 
 interface MovieProps {
     movie: MovieType;
+
 }
 
 const Movie: React.FC<MovieProps> = ({ movie }) => {
-    return (
-        <div className="movie-card">
-            {
-                movie.poster_path
-                    ? <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} loading="lazy" alt={movie.title} />
-                    : <h3>No Image Available</h3>
-            }
 
-            {/* <h3>{movie.title}</h3> */}
-        </div>
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        <Link to={`/movie/${movie.id}`}>
+
+            <div className="movie-card"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                {
+                    movie.poster_path
+                        ? <img className="movie-poster" src={`https://image.tmdb.org/t/p/w400${movie.backdrop_path}`} loading="lazy" alt={movie.title} />
+                        : <h3>No Image Available</h3>
+                }
+                {/* <div className="movie-title">{movie.title}</div> */}
+
+
+                {
+                    isHovered ? (
+                        <div className="movie-preview">
+                            <h3>{movie.title}</h3>
+                            <p>{movie.overview}</p>
+
+                        </div>
+                    ) :
+                        (<div className="movie-title">{movie.title}</div>)
+
+                }
+
+
+
+                {/* <h3>{movie.title}</h3> */}
+            </div>
+        </Link>
+
     );
 }
 
