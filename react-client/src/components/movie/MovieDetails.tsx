cb5f527d044dd61b0e086988383bb44213a39ab4
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-13 17:10:11 -0700

    change stylings for movie details & searchbar

--- react-client/src/components/movie/MovieDetails.tsx
+++ react-client/src/components/movie/MovieDetails.tsx
@@ -4,6 +4,8 @@ import { backendUrl } from '../../utils/config';
 import { MovieType } from '../../types/movie';
 import { useState, useEffect } from 'react';
 import { CircularProgress } from '@mui/material';
+import './movie-details.scss';
+
 
 const MovieDetails: React.FC = () => {
     const [movie, setMovie] = useState<MovieType | null>(null);
@@ -36,30 +38,35 @@ const MovieDetails: React.FC = () => {
     if (!movie) return <CircularProgress />;
 
     return (
-
         <div className="movie-details">
             <h1>{movie.title}</h1>
-            {
-                movie.poster_path
-                    ? <img className="movie-poster" src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} loading="lazy" alt={movie.title} />
-                    : <h3>No Image Available</h3>
-            }
-            {movie.tagline && <p><em>{movie.tagline}</em></p>}
-            <p><strong>Release Date:</strong> {new Date(movie.release_date).toLocaleDateString(undefined, { month: 'long', day: 'numeric', year: 'numeric', })}</p>
-            <p><strong>Description:</strong> {movie.overview}</p>
-            <p><strong>Audience Score:</strong> {movie.vote_average} ({movie.vote_count} votes)</p>
-            {movie.certification && <p><strong>Content Rating:</strong> {movie.certification}</p>}
-            <p><strong>Runtime:</strong> {movie.runtime} minutes</p>
-            {/* {movie.genres && <p><strong>Genres:</strong> {movie.genres.map(genre => genre.name).join(', ')}</p>} */}
-            {movie.genres && <p><strong>Genres:</strong> {movie.genres.join(', ')}</p>}
-            {/* {movie.spoken_languages && <p><strong>Languages:</strong> {movie.spoken_languages.map(lang => lang.name).join(', ')}</p>} */}
-            {movie.spoken_languages && <p><strong>Languages:</strong> {movie.spoken_languages.join(', ')}</p>}
-
-            {movie.actors && <p><strong>Cast:</strong> {movie.actors.join(', ')}</p>}
-            {movie.directors && <p><strong>Director:</strong> {movie.directors.join(', ')}</p>}
-
+            <div className="content">
+                <div className="image-container">
+                    {movie.poster_path
+                        ? <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} loading="lazy" alt={movie.title} />
+                        : <h3>No Image Available</h3>}
+                </div>
+                <div className="details-container">
+                    {movie.tagline && <p className="tagline"><em>{movie.tagline}</em></p>}
+                    <p><strong className="description-label">Description:</strong></p>
+                    <p className="description">{movie.overview}</p>
+                    <div className="quick-details">
+                        <p><strong>Release Date:</strong> {new Date(movie.release_date).toLocaleDateString(undefined, { month: 'long', day: 'numeric', year: 'numeric' })}</p>
+                        <p><strong>Audience Score:</strong> {movie.vote_average} ({movie.vote_count} votes)</p>
+                        <p><strong>Runtime:</strong> {movie.runtime} minutes</p>
+                        {movie.certification && <p><strong>Content Rating:</strong> {movie.certification}</p>}
+                    </div>
+                    <div className="additional-details">
+                        {movie.genres && <p><strong>Genres:</strong> {movie.genres.join(', ')}</p>}
+                        {movie.spoken_languages && <p><strong>Languages:</strong> {movie.spoken_languages.join(', ')}</p>}
+                        {movie.actors && <p><strong>Cast:</strong> {movie.actors.join(', ')}</p>}
+                        {movie.directors && <p><strong>Director:</strong> {movie.directors.join(', ')}</p>}
+                    </div>
+                </div>
+            </div>
         </div>
     );
+
 }
 
 export default MovieDetails;
