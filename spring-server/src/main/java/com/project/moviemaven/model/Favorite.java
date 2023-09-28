000ccff03f20b0cc72c069c6e5187e7c445eb052
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-28 01:03:54 -0700

    using @JoinTable for many-to-many relationship between User and Movie in User class for simplification

--- spring-server/src/main/java/com/project/moviemaven/model/Favorite.java
+++ spring-server/src/main/java/com/project/moviemaven/model/Favorite.java
@@ -1,36 +1,36 @@
-package com.project.moviemaven.model;
+// package com.project.moviemaven.model;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "favorites")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class Favorite {
+// @Entity
+// @Table(name = "favorites")
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Data
+// public class Favorite {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+//     @ManyToOne
+//     @JoinColumn(name = "user_id")
+//     private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "movie_id")
-    private Movie movie;
+//     @ManyToOne
+//     @JoinColumn(name = "movie_id")
+//     private Movie movie;
 
-    private LocalDateTime date;
-}
+//     private LocalDateTime date;
+// }
--- spring-server/src/main/java/com/project/moviemaven/repository/FavoriteRepository.java
+++ spring-server/src/main/java/com/project/moviemaven/repository/FavoriteRepository.java
@@ -1,18 +1,18 @@
-package com.project.moviemaven.repository;
+// package com.project.moviemaven.repository;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import com.project.moviemaven.model.Favorite;
+// import com.project.moviemaven.model.Favorite;
 
 
-@Repository
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    List<Favorite> findByUserId(Long userId); //user favorites
+// @Repository
+// public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+//     List<Favorite> findByUserId(Long userId); //user favorites
 
-    Optional<Favorite> findByUserIdAndMovieId(Long userId, Long movieId);
+//     Optional<Favorite> findByUserIdAndMovieId(Long userId, Long movieId);
 
-}
+// }
