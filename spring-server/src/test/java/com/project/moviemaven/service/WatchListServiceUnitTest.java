8aa36204e2cb511f28153b2c523295ac6cf30f39
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-28 12:55:55 -0700

    add unit test cases for addWatchList & removeFromWatchList and update getWatchList

--- spring-server/src/test/java/com/project/moviemaven/service/WatchListServiceUnitTest.java
+++ spring-server/src/test/java/com/project/moviemaven/service/WatchListServiceUnitTest.java
@@ -1,12 +1,14 @@
 package com.project.moviemaven.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,58 +17,110 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.project.moviemaven.dto.MovieDTO;
 import com.project.moviemaven.model.Movie;
-import com.project.moviemaven.model.Role;
 import com.project.moviemaven.model.User;
+import com.project.moviemaven.repository.MovieRepository;
+import com.project.moviemaven.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class WatchListServiceUnitTest {
 
     @Mock
-    UserService userService; // simulate the behavior
+    private UserService userService; // simulate the behavior
 
     @Mock
-    MovieService movieService; // simulate the behavior
+    private MovieService movieService; // simulate the behavior
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private MovieRepository movieRepository;
 
     @InjectMocks
-    WatchListService watchListService;
+    private WatchListService watchListService;
+
+    private User user;
+    private Movie movie1;
+    private Movie movie2;
+
+    // helper
+    private Movie createMovie(Long tmdbId, String title, String releaseDate) {
+        return Movie.builder()
+                .id(tmdbId)
+                .tmdbId(tmdbId)
+                .title(title)
+                .releaseDate(releaseDate)
+                .build();
+    }
 
     @BeforeEach
     public void setup() {
-        //
+        user = User.builder()
+                .id(1L)
+                .username("username")
+                .password("password")
+                .build();
+
+        movie1 = createMovie(1L, "superman", "2005-06-15");
+        movie2 = createMovie(2L, "batman", "2008-07-18");
+
     }
 
     @Test
     public void testAddWatchList() {
-        // TODO
+        Long tmdbId = 1L;
+
+        // mock the behavior of userService and movieService
+        when(userService.getUserByUsername("username")).thenReturn(user);
+        when(movieService.getOrAddMovieToDb(tmdbId)).thenReturn(movie1);
+
+        // test
+        watchListService.addToWatchList("username", tmdbId);
+
+        assertTrue(user.getWatchList().contains(movie1));
+        assertTrue(movie1.getUserWatchlist().contains(user));
+
+        verify(userRepository).save(any(User.class));
+        verify(movieRepository).save(any(Movie.class));
     }
 
     @Test
     public void testGetWatchList() {
-        Long userId = 1L;
-        Long movieId = 2L;
-        Long movieId2 = 3L;
+        String username = "username";
 
-        Set<User> users = new HashSet<>();
+        user.getWatchList().add(movie1);
+        user.getWatchList().add(movie2);
 
-        Movie movie1 = new Movie(1L, movieId, "superman", "2005-06-15", users);
-        Movie movie2 = new Movie(2L, movieId2, "batman", "2008-07-18", users);
+        // mock
+        when(userService.getUserByUsername(username)).thenReturn(user);
 
-        Set<Movie> watchList = new HashSet<>();
-        watchList.add(movie1);
-        watchList.add(movie2);
+        // test
+        List<MovieDTO> movieDTOList = watchListService.getWatchList(username);
 
-        User user = new User(userId, "username", "password", watchList, Role.ROLE_USER);
-
-        // mock the behavior of userService and movieService
-        when(userService.getUserById(userId)).thenReturn(user);
-        when(movieService.getMovieFromDb(movieId)).thenReturn(movie1);
-        when(movieService.getMovieFromDb(movieId2)).thenReturn(movie2);
-
-        List<Movie> movieList = watchListService.getWatchList(user.getId());
-        assertEquals(movieList.size(), 2);
-        assertEquals(movieList.get(0).getTitle(), "superman");
-        assertEquals(movieList.get(1).getTitle(), "batman");
-
+        assertEquals(movieDTOList.size(), 2);
+        assertEquals(movieDTOList.get(0).getTitle(), "superman");
+        assertEquals(movieDTOList.get(1).getTitle(), "batman");
     }
+
+    @Test
+    public void testRemoveFromWatchList() {
+        String username = "username";
+        Long tmdbId = 200L;
+
+        user.getWatchList().add(movie1);
+        movie1.getUserWatchlist().add(user); // Since it's bi-directional with Movie
+
+        // mock
+        when(userService.getUserByUsername(username)).thenReturn(user);
+        when(movieRepository.findByTmdbId(tmdbId)).thenReturn(Optional.of(movie1));
+
+        // test
+        watchListService.removeFromWatchList(username, tmdbId);
+
+        assertFalse(user.getWatchList().contains(movie1));
+        assertFalse(movie1.getUserWatchlist().contains(user));
+    }
+
 }
