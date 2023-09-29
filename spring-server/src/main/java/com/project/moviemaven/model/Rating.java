5ad2f0b36c653d2b84cf62330bd3d9516aabba61
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-09-28 20:18:27 -0700

    add Lombok Builder annotation

--- spring-server/src/main/java/com/project/moviemaven/model/Rating.java
+++ spring-server/src/main/java/com/project/moviemaven/model/Rating.java
@@ -17,6 +17,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -26,6 +27,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
