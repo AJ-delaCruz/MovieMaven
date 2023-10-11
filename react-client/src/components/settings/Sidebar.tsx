736b76aad0006a2473a5b9ff3a9959f15dfa0cac
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-10-11 14:38:12 -0700

    adjust sidebar UI layout for consistent display on different monitor resolutions

--- react-client/src/components/settings/Sidebar.tsx
+++ react-client/src/components/settings/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { List, ListItemButton, ListItemText, Drawer, Typography } from '@mui/material';
+import { List, ListItemButton, ListItemText, Typography } from '@mui/material';
 import './settings.scss';
 
 const Sidebar: React.FC = () => {
@@ -7,29 +7,24 @@ const Sidebar: React.FC = () => {
 
     return (
         <div className='sidebar' >
-            <Drawer
-                variant="permanent"
-                anchor="left"
-            >
-                <div className='header'>
-                    <Typography variant="h6">Settings</Typography>
-                </div>
-                {/* <Divider sx={{ color: 'red', backgroundColor: '#282828' }} /> */}
-                <List>
-                    <ListItemButton component={Link} to="/settings/profile" selected={location.pathname === "/settings/profile"}>
-                        <ListItemText primary="Edit Profile" />
-                    </ListItemButton>
-                    <ListItemButton component={Link} to="/settings/preferences" selected={location.pathname === "/settings/preferences"}>
-                        <ListItemText primary="Preferences" />
-                    </ListItemButton>
-                    <ListItemButton component={Link} to="/settings/notification" selected={location.pathname === "/settings/notification"}>
-                        <ListItemText primary="Notifications" />
-                    </ListItemButton>
-                    <ListItemButton component={Link} to="/settings/delete" selected={location.pathname === "/settings/delete"}>
-                        <ListItemText primary="Delete Account" />
-                    </ListItemButton>
-                </List>
-            </Drawer>
+            <div className='header'>
+                <Typography variant="h6">Settings</Typography>
+            </div>
+            {/* <Divider sx={{ color: 'red', backgroundColor: '#282828' }} /> */}
+            <List>
+                <ListItemButton component={Link} to="/settings/profile" selected={location.pathname === "/settings/profile"}>
+                    <ListItemText primary="Edit Profile" />
+                </ListItemButton>
+                <ListItemButton component={Link} to="/settings/preferences" selected={location.pathname === "/settings/preferences"}>
+                    <ListItemText primary="Preferences" />
+                </ListItemButton>
+                <ListItemButton component={Link} to="/settings/notification" selected={location.pathname === "/settings/notification"}>
+                    <ListItemText primary="Notifications" />
+                </ListItemButton>
+                <ListItemButton component={Link} to="/settings/delete" selected={location.pathname === "/settings/delete"}>
+                    <ListItemText primary="Delete Account" />
+                </ListItemButton>
+            </List>
         </div >
     );
 };
