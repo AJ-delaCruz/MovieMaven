d1038a0d31f76ffbf94969a22c1b7869a1388a64
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-10-10 19:35:04 -0700

    fix path

--- react-client/src/utils/config.ts
+++ react-client/src/utils/config.ts
@@ -1,1 +1,1 @@
-export const backendUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
+export const backendUrl = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000';
